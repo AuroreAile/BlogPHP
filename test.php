bcddb8d39f289f31e4b77d1e25fc5faf94c0726a
AuroreAile <auroredelenclos@yahoo.fr>
2024-01-15 22:27:21 +0100

    fin test

--- test.php
+++ test.php
@@ -1,5 +1,25 @@
-<?php
+<?php 
 
-    echo 'Hello!';
+$firstName = "John";
+$age = 12;
+$prix = 12.5;
+
+
+/*if ($age >= 18) {
+    echo "Vous êtes majeur";
+} elseif ($age >= 13) {
+    echo "Vous êtes ado";
+}
+
+else {
+    echo "Vous êtes mineur";
+
+}
+*/
+
+//tableau simple
+$fruits = ["pomme", "poire", "banane"];
+echo $fruits;
+
 
 ?>
