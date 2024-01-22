4fe74bff33af94622d7d5f0c7c5e0de667264833
AuroreAile <auroredelenclos@yahoo.fr>
2024-01-22 22:22:09 +0100

    htmlentities

--- projet/actualite.php
+++ projet/actualite.php
@@ -7,7 +7,7 @@ $article = $articles[$id];
 
 
 require_once "../lib/menu.php";
-$mainMenu["actualite.php"] = ["head_title" => $article["title"], "meta-description" => substr($article["content"], 0, 250), "exclude"=> true];
+$mainMenu["actualite.php"] = ["head_title" => htmlentities ($article["title"]), "meta-description" => htmlentities (substr ($article["content"], 0, 250)), "exclude"=> true];
 //raccourcir meta-description ! substr($article["content"], 0, 150 caractères)
 require_once "../templates/header.php";
 ?>
@@ -20,14 +20,27 @@ require_once "../templates/header.php";
 <div class="container col-xxl-8 px-4 py-5">
   <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
     <div class="col-10 col-sm-8 col-lg-6">
-      <img src="/uploadsarticles/<?= $article['image'] ?>" class="d-block mx-lg-auto img-fluid" alt="ALT ARTICLE" width="700" height="500" loading="lazy">
+      <img src="/uploadsarticles/<?= htmlentities ($article['image']) ?>" class="d-block mx-lg-auto img-fluid" alt="ALT ARTICLE" width="700" height="500" loading="lazy">
     </div>
     <div class="col-lg-6">
-      <h1 class="display-5 fw-bold lh-1 mb-3"><?php echo $article['title']; ?></h1>
-      <p class="lead"><?php echo $article['content']; ?></p>
+      <h1 class="display-5 fw-bold lh-1 mb-3"><?= htmlentities ($article['title']); ?></h1>
+      <p class="lead"><?= htmlentities ($article['content']); ?></p>
     </div>
   </div>
 </div>
+
+<div class="row g-5 py-5">
+  <form method="post">
+    <p>
+      <label for="comment">Ajouter un commentaire</label><br>
+      <textarea name="comment" id="comment" cols="30" rows="10"></textarea>
+      
+    </p>
+    <input type="submit" value="Envoyer">
+  </form>
+  <h2>Votre commentaire</h2>
+  <p><?= htmlentities($_POST["comment"]); ?></p> <!--recupérer le commentaire via POST comme c'est un formulaire post-->
+</div> <!--transformer toutes les entités html en équivalent caractères => script n'a plus d'effet. protége toutes les donnes qui viennent de l'extérieur -->
 
 
 
